088a165624fb5d8f7a6df7f72a0f1434c18b45b9
Simon Groenborg <groenborg27@gmail.com>
2017-03-17 14:55:06 +0100

    release git-phlow version 1.0.6

--- Formula/git-phlow.rb
+++ Formula/git-phlow.rb
@@ -1,9 +1,9 @@
 class GitPhlow < Formula
   desc "pragmatic workflow extension for git"
   homepage "https://github.com/Praqma/git-phlow"
-  url "https://github.com/Praqma/git-phlow/releases/download/v1.0.0/git-phlow-1.0.0-darwin-amd64.tar.gz"
-  version "1.0.0"
-  sha256 "aad1c83268d97c43e4efe794fa46ce50a5c1ffe552d9117bd645cbb783515f66"
+  url "https://github.com/Praqma/git-phlow/releases/download/v1.0.6/git-phlow-1.0.6-darwin-amd64.tar.gz"
+  version "1.0.6"
+  sha256 "9e08763b21b9dcd211b1fc3bc4c723e204d7c26b71323db53afc6c088a7946df"
 
   def install
     bin.install "git-phlow"
