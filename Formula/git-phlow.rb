f6c617a57eddfdcd67291caf549a731d7c83bddc
concourse <concourse@praqma.net>
2017-06-22 10:20:01 +0000

    1.1.5 released

--- Formula/git-phlow.rb
+++ Formula/git-phlow.rb
@@ -4,7 +4,7 @@ class GitPhlow < Formula
   homepage "https://github.com/Praqma/git-phlow"
   url "https://github.com/Praqma/git-phlow/releases/download/v1.1.5/git-phlow-1.1.5-darwin-amd64.tar.gz"
   version "1.1.5"
-  sha256 "668b3c9ef7ea801962256ad9dc5554fcf9af443afc87d27a2d5457ff3f9eb2ff"
+  sha256 "2729a9168595a263e4a01f44e734e3cce4160d250848281b600acc5ad05f941b"
 
   def install
     bin.install "git-phlow"
